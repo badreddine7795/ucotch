247b83f5354a1b8fd2f9fdbb50c3f1ce72e8fcfd
badreddine7795 <badreddine7795@gmail.com>
2019-12-25 13:52:28 +0100

    badr_branch_changes

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -2,37 +2,43 @@ import React from 'react';
 import {
     View,
     TouchableOpacity,
-
+    Image,
+    Text,
     StyleSheet
 } from 'react-native';
-import {
-    ExpoLinksView
-} from '@expo/samples';
 
 export default function SignInScreen() {
     return (
-        < View style={styles.container} >
-            <TouchableOpacity onPress={this._onPressButton}>
-      <Image
-        style={styles.button}
-        source={require('./https://img.utdstc.com/icons/facebook-android.png:s')}
-      />
-    </TouchableOpacity>
-    <TouchableOpacity onPress={this._onPressButton}>
-      <Image
-        style={styles.button}
-        source={require('./https://www.presse-citron.net/wordpress_prod/wp-content/uploads/2018/12/google-g-logo.jpg')}
-      />
-    </TouchableOpacity>
 
-           
+        < View style={styles.container} >
+            <TouchableOpacity onPress={() => console.log("olo")}>
+                <Image
+                    style={styles.button}
+                    source={
+                        {
+                            uri: 'https://img.utdstc.com/icons/facebook-android.png:s',
+                        }}
+
+                />
+                <Text>sign in with</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => console.log("olo")}>
+                <Image
+                    style={styles.button}
+                    source={
+                        {
+                            uri: 'https://www.presse-citron.net/wordpress_prod/wp-content/uploads/2018/12/google-g-logo.jpg',
+                        }}
+                />
+                <Text>sign in with</Text>
+            </TouchableOpacity>
+
+
         </View>
+
     );
 }
 
-LinksScreen.navigationOptions = {
-    title: 'Links',
-};
 
 const styles = StyleSheet.create({
     container: {
@@ -40,4 +46,8 @@ const styles = StyleSheet.create({
         paddingTop: 15,
         backgroundColor: '#fff',
     },
+    button: {
+        width: 50,
+        height: 50
+    }
 });
